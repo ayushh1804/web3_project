55e2232a977352899040054d642311edc16dbf0f
AYUSH <ayushrajbls@gmail.com>
2023-04-05 02:17:02 +0530

    Added Navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { HiMenuAlt4 } from "react-icons/hi";
 
 import logo from "../../images/logo.png";
+
+const NavbarItem = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
+
 const Navbar = () => {
   return (
-<nav className='w-full flex md:justify-center justify-between items-center p-4'>
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className=" md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+      </div>
+      <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+        {["Market","Exchange","Tutorials","Wallets"].map((item, index) => (
+          <NavbarItem key={item + index} title={item}/>
+        ))}
+        <li className="bg-[#2952e3] py-2 px-7 cursor-pointer rounded-full hover:bg-[#2546bd]">Login</li>
+      </ul>
+    </nav>
+  );
+};
 
-  
-</nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
