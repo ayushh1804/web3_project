538cca536597adc9b9182f4eb9e9731243553b6b
AYUSH <ayushrajbls@gmail.com>
2023-04-05 01:44:49 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Services from "./Components/Services";
+import Transactions from "./Components/Transactions";
+import Welcome from "./Components/Welcome";
 
 function App() {
-  return <div className="text-red-600">hii</div>;
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
