2aad1a2590f4e21d0dfca6d1862d6275a9b469e8
AYUSH <ayushrajbls@gmail.com>
2023-04-05 03:16:32 +0530

    changes

--- client/src/Components/Welcome.jsx
+++ client/src/Components/Welcome.jsx
@@ -4,12 +4,15 @@ import { BsInfoCircle } from "react-icons/bs";
 import { SiEthereum } from "react-icons/si";
 
 // import { Loader } from "./Loader"
-const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
-const Welcome = () => {
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const connectWallet = () => {
-
+const Input = () => {
+  
 }
+
+const Welcome = () => {
+  const connectWallet = () => {};
 
   return (
     <div className="flex w-full justify-center items-center">
@@ -20,23 +23,52 @@ const connectWallet = () => {
             <br />
             across the world
           </h1>
-          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy and Sell CryptoCurrencies on Krypto</p>
-          <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546b3]"><p className="text-white text-base font-semibold">Connect wallet</p></button>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+            Explore the crypto world. Buy and Sell CryptoCurrencies on Krypto
+          </p>
+          <button
+            type="button"
+            onClick={connectWallet}
+            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546b3]"
+          >
+            <p className="text-white text-base font-semibold">Connect wallet</p>
+          </button>
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10 ">
-
-            <div className={`rounded-tl-2xl ${commonStyles }`}>Reliability</div>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
             <div className={commonStyles}>Security</div>
-            
+
             <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
-            <div className={`rounded-bl-2xl ${commonStyles }`}>Web 3.0</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
             <div className={commonStyles}>Low Fees</div>
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
         </div>
 
+        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10 ">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center ">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">
+                  0x23434huh.....43334hh
+                </p>
+              </div>
+              <div>
+                <p className="text-white font-semibold mt-1 text-lg">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
 
-        
+          <div className="p-5 w-full flex flex-col sm:w-96 justify-start items-center blue-glassmorphism"></div>
+        </div>
       </div>
     </div>
   );
