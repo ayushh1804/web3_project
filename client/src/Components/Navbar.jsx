671cc3b89a36c13c6eae64d23f7a5eb830cf4e30
AYUSH <ayushrajbls@gmail.com>
2023-04-05 02:05:22 +0530

    Added package ether

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineClose } from "react-icons/ai";
+import { HiMenuAlt4 } from "react-icons/hi";
 
+import logo from "../../images/logo.png";
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+<nav className='w-full flex md:justify-center justify-between items-center p-4'>
+
+  
+</nav>
   )
 }
 
